34b66cca229c14e0d6502c8fb6f66d0d58442015
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-04-11 12:00:48 +0530

    Day2

---
+++ Day2/CountNodes.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        return 1+countNodes(root.left)+countNodes(root.right);
+    }
+}
+
+//adcanced soln
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution { //advanced solution -> less than O(n) Time Complexity 
+    public int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        int lh = getLeftHt(root);
+        int rh = getRightHt(root);
+
+        if(lh==rh){
+            return (int)Math.pow(2, lh) - 1;
+        }else{
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+
+    public static int getLeftHt(TreeNode root){
+        int ct = 1;
+        while(root.left!=null){
+            ct++;
+            root = root.left;
+        }
+        return ct;
+    }
+
+    public static int getRightHt(TreeNode root){
+        int ct = 1;
+        while(root.right!=null){
+            ct++;
+            root = root.right;
+        }
+        return ct;
+    }
+}
+
+// tc -> o(logn^2)
+
+
+//write main 
---
+++ Day2/NodesAtDistK.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static void markParents(HashMap<TreeNode, TreeNode> parentMap, TreeNode root){
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll();
+            if(curr.left!=null){
+                parentMap.put(curr.left, curr);
+                q.offer(curr.left);
+            }
+            if(curr.right!=null){
+                parentMap.put(curr.right, curr);
+                q.offer(curr.right);
+            }
+        }
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        HashMap<TreeNode, TreeNode> parentMap = new HashMap<>();
+        markParents(parentMap, root);
+        HashMap<TreeNode, Boolean> vis = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(target);
+        vis.put(target, true);
+        int currLvl = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            if(currLvl==k){
+                break;
+            }
+            currLvl++;
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(curr.left!=null && vis.get(curr.left)==null){
+                    q.offer(curr.left);
+                    vis.put(curr.left, true);
+                }
+                if(curr.right!=null && vis.get(curr.right)==null){
+                    q.offer(curr.right);
+                    vis.put(curr.right, true);
+                }
+                if(parentMap.get(curr)!=null && vis.get(parentMap.get(curr))==null){
+                    q.offer(parentMap.get(curr));
+                    vis.put(parentMap.get(curr), true);
+                }
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll();
+            res.add(curr.val);
+        }
+        return res;
+    }
+}
+
+//write main to run and check
