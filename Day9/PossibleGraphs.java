7baf63c9607a5f84816a18995baa3faf28d15441
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-04-21 20:22:37 +0530

    Day9

---
+++ Day9/PossibleGraphs.java
@@ -0,0 +1,21 @@
+/*
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+*/
+//coding ninjas 
+//striver sheet graph basics  Graph and Types
+public class Solution {
+    public static int countingGraphs(int N) {
+        // Declaring 'ans' & 'E'.
+        int ans,E;
+
+        // Calculating the total number of edges possible in the graph.
+        E = (N * (N-1)) / 2;
+
+        // Calculating the total number of graphs.
+        ans = 1 << E;
+
+        // Returning 'ans'.
+        return ans;
+    }
+}
