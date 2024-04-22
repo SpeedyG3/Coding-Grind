0fb1257e90846b43bb65b25da5ec38313e81b608
Hrishi <hrishikeshkulkarni009@gmail.com>
2024-04-23 01:48:39 +0530

    Day10

--- Day10/GraphDFS.java
+++ Day10/GraphDFS.java
@@ -12,17 +12,18 @@ class GFG {
             String[] s = br.readLine().trim().split(" ");
             int V = Integer.parseInt(s[0]);
             int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
             for (int i = 0; i < E; i++) {
                 String[] S = br.readLine().trim().split(" ");
                 int u = Integer.parseInt(S[0]);
                 int v = Integer.parseInt(S[1]);
                 adj.get(u).add(v);
-                // adj.get(v).add(u);
+                adj.get(v).add(u);
             }
             Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
             for (int i = 0; i < ans.size(); i++)
                 System.out.print(ans.get(i) + " ");
             System.out.println();
@@ -31,34 +32,26 @@ class GFG {
 }
 
 // } Driver Code Ends
-// a2z
-//striver GFG coding 
-//GRAPH BFS 
+
+//striver GFG coding a2z
+//GRAPH DFS 
 class Solution {
-    // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> bfs = new ArrayList<>();
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> list = new ArrayList<>();
         boolean vis[] = new boolean[V];
-        Queue<Integer> q = new LinkedList<>();
+        dfs(0, list, vis, adj);
+        return list;
         
-        q.add(0);
-        vis[0] = true;
-        
-        while(!q.isEmpty()){
-            int node = q.poll();
-            bfs.add(node);
-            
-            //get all adj elemenets of node now
-            //only if not visited then add to queue
-            //mark in visited aswell 
-            for(int i: adj.get(node)){
-                if(!vis[i]){
-                    vis[i] = true;
-                    q.add(i);
-                }
+    }
+    
+    public static void dfs(int node, ArrayList<Integer> list, boolean vis[], ArrayList<ArrayList<Integer>> adj){
+        vis[node] = true;
+        list.add(node);
+        for(int i: adj.get(node)){
+            if(!vis[i]){
+                dfs(i, list, vis, adj);
             }
         }
-        
-        return bfs;
     }
 }
